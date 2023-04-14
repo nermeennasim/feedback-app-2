cc1de0b4f21875cd4edbea3ef2be1668171e132a
NERMEEN NASIM <nermeen.nasim@hotmail.com>
2023-04-14 01:20:09 -0700

    commit to change redirect and netlify
    adding app.js as path

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Card from "./shared/Card";
 import PropTypes from "prop-types";
 import { FaTimes, FaEdit } from "react-icons/fa";
